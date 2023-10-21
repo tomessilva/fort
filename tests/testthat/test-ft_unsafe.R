862d7b5bf2aa0f5d28d9b08d6111e7e5188fcd86
t.s.silva <tome@tomesilva.com>
2023-10-21 17:49:18 +0100

    Finish testing for unsafe operator (100% coverage)

--- tests/testthat/test-ft_unsafe.R
+++ tests/testthat/test-ft_unsafe.R
@@ -11,3 +11,19 @@ test_that("the %***%.FastTransform operator works correctly", {
     expect_equal(result1,result3,tolerance=tol_)
   }
 })
+
+test_that("the %***%.FastTransform operator works for inverse transforms", {
+  tests <- list(c(2,2),
+                c(2,3),
+                c(3,2),
+                c(8,8))
+  tol_ <- 10^-8
+  for (cur_size in tests) {
+    tmp_fort <- fort(cur_size)
+    tmp_data <- diag(cur_size[2])
+    tmp_fort_inv <- solve(tmp_fort)
+    tmp_result1 <- tmp_fort_inv %***% tmp_data
+    tmp_result2 <- tmp_fort_inv %*% tmp_data
+    expect_equal(tmp_result1,tmp_result2,tolerance=tol_)
+  }
+})
